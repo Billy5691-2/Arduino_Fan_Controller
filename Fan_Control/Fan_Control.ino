0e1b7a0dc50618a053d6cfdf574f28f83f8274da
Billy M <Billymortimer1965@gmail.com>
2025-01-31 20:09:24 +0000

    Corrected PWM control for Uno R4 Wifi

--- Fan_Control/Fan_Control.ino
+++ Fan_Control/Fan_Control.ino
@@ -1,46 +1,73 @@
+#include <pwm.h>
 
 // Temperature control variables
 const int thermistorPin = 0; //Pin of Thermistor output
 int rawTempVoltage; // Raw output from thermistor pin
-const loat r1 = 10000; // Fixed resistor value, 10,000 Ohms
+const float r1 = 10000; // Fixed resistor value, 10,000 Ohms
 float logR2, r2, temp;
 const float A = 1.009249522e-03, B = 2.378405444e-04, C = 2.019202697e-07; //Stein Heart Equation Values
 
 
 // Fan control variables
-const int PWM_FREQ_HZ = 25000;
-const int OC1B_PIN = 10;
-const int temp_lower = 20;
-const int temp_upper = 40;
+const word PWM_FREQ_HZ = 25000;
+const byte GTIOC7B_PIN = 9;
+//const word TCNT1_TOP = 16000000/(2*PWM_FREQ_HZ);
+
+PwmOut pwm(D9);
+
+//const byte OC1B_PIN = 10;
+//const int SETTEMPINTERVAL = 30000;
+
+
+const int Temp_Lower = 20;
+const int Temp_Upper = 40;
 float PWMDuty;
-int PWMDuty_int = static_cast<int>(PWMDuty);
+int PWMDuty_int;
+//int PWMDuty_int = static_cast<int>(PWMDuty);
 
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+
+  // Clear Timer Settings:
+  //GTIOC7B = 0;
+  //TCCR1B = 0;
+  //TCNT1 = 0;
+
+  //TCCR1A |= (1 << COM1A1) | (1 << WGM11);
+  //TCCR1B |= (1 << WGM13) | (1 << CS10);
+  //ICR1 = TCNT1_TOP;
   
-  pinMode(OC1B_PIN, OUTPUT);
+  //pinMode(GTIOC7B_PIN, OUTPUT);
+  pwm.begin(PWM_FREQ_HZ, 0.0f);
   PWMDuty = 20.0;
-  setPwmDuty(PWMDuty);
+  delay(3000);
+  setPWMDuty(PWMDuty);
   delay(2000);
 
 }
 
 void temperature() {
   //Reads the voltage from an analog pin connected to a thermistor and converts it to a temperature in degrees C
-  rawTempVoltage = analog.Read(thermistorPin);
+  rawTempVoltage = analogRead(thermistorPin);
 
   r2 = r1 * (1023.0 / (float)rawTempVoltage - 1);
   logR2 = log(r2);
   temp = (1.0 / (A + B*logR2 + C*logR2*logR2*logR2));
   temp = temp - 273.15;
   Serial.print("Temperature: ");
-  Serial.print(T);
+  Serial.print(temp);
   Serial.println(" C");
 
   //return temp;
+}
+
+void setPWMDuty(float duty){
+  //OCR1A = (word) (duty*TCNT1_TOP)/100;
+  //analogWrite(GTIOC7B_PIN, (int) (duty*TCNT1_TOP)/100);
+  pwm.pulse_perc(duty);
 }
 
 void fanControl() {
@@ -50,17 +77,18 @@ void fanControl() {
   //Above 40C, the fan runs at full speed
   //In between, the fan runs at a relative value between 20 and 100%
 
-  if (temp < temp_lower) {
+  if (temp < Temp_Lower) {
     PWMDuty = 20.0;
   } 
-  else if (temp > temp_upper) {
-  }     
+  else if (temp > Temp_Upper) {     
     PWMDuty = 100.0;
-  else {
-    PWMDuty = 20 + (temp / temp_upper * 80);
-    
   }
-  setPWMDuty(PWMDuty_int);
+  else {
+    PWMDuty = 20 + ((temp - Temp_Lower) / (Temp_Upper - Temp_Lower) * 80);
+  }
+  PWMDuty_int = static_cast<int>(PWMDuty);
+
+  setPWMDuty(PWMDuty);
   Serial.print("PWM Duty: ");
   Serial.println(PWMDuty_int);
 }
@@ -72,6 +100,11 @@ void loop() {
 
   temperature();
   fanControl();
+  delay(500);
 
 
 }
+
+
+
+
