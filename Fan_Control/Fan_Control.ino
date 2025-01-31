9084216165a17160e4af5a39d57977d24e8d9bf4
Billy M <Billymortimer1965@gmail.com>
2025-01-31 22:24:47 +0000

    Added rolling temperature measurement

--- Fan_Control/Fan_Control.ino
+++ Fan_Control/Fan_Control.ino
@@ -4,48 +4,44 @@
 const int thermistorPin = 0; //Pin of Thermistor output
 int rawTempVoltage; // Raw output from thermistor pin
 const float r1 = 10000; // Fixed resistor value, 10,000 Ohms
-float logR2, r2, temp;
+float logR2, r2, temp, avgTemp;
 const float A = 1.009249522e-03, B = 2.378405444e-04, C = 2.019202697e-07; //Stein Heart Equation Values
+
+const int RT_Len = 10;
+float rollingTemp[RT_Len];
+int tempIndex = 0;
+//float rolling_temp[RT_Len] = {0.0, 0.0, 0.0, 0.0, 0.0};
+//std::fill(std::begin(rolling_temp), std::end(rolling_temp), 0);
 
 
 // Fan control variables
 const word PWM_FREQ_HZ = 25000;
-const byte GTIOC7B_PIN = 9;
-//const word TCNT1_TOP = 16000000/(2*PWM_FREQ_HZ);
-
 PwmOut pwm(D9);
 
-//const byte OC1B_PIN = 10;
-//const int SETTEMPINTERVAL = 30000;
 
 
-const int Temp_Lower = 20;
-const int Temp_Upper = 40;
+
+const int TEMP_MIN = 2;
+const int TEMP_MAX = 40;
 float PWMDuty;
 int PWMDuty_int;
-//int PWMDuty_int = static_cast<int>(PWMDuty);
 
 
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
 
-  // Clear Timer Settings:
-  //GTIOC7B = 0;
-  //TCCR1B = 0;
-  //TCNT1 = 0;
-
-  //TCCR1A |= (1 << COM1A1) | (1 << WGM11);
-  //TCCR1B |= (1 << WGM13) | (1 << CS10);
-  //ICR1 = TCNT1_TOP;
-  
-  //pinMode(GTIOC7B_PIN, OUTPUT);
-  pwm.begin(PWM_FREQ_HZ, 0.0f);
+  pwm.begin(PWM_FREQ_HZ, 20.0f);
   PWMDuty = 20.0;
-  delay(3000);
-  setPWMDuty(PWMDuty);
   delay(2000);
+
+  Serial.print("Program Start");
+  Serial.println("");
+
+
+  //for (int i=0; i < RT_Len; i++) rolling_temp[i] = 0;
+
+  std::fill(std::begin(rollingTemp), std::end(rollingTemp), 0);
 
 }
 
@@ -60,13 +56,29 @@ void temperature() {
   Serial.print("Temperature: ");
   Serial.print(temp);
   Serial.println(" C");
+}
 
-  //return temp;
+void update_avg_temp() {
+  //Updates the rolling average temp;
+  float total = 0;
+  rollingTemp[tempIndex] = temp;
+  
+  for (int i=0; i<RT_Len; i++){
+    total += rollingTemp[i];
+  }
+
+  tempIndex ++;
+  if (tempIndex > RT_Len) tempIndex = 0;
+  
+  avgTemp = total / RT_Len;
+
+  Serial.print("Average Temperature: ");
+  Serial.print(avgTemp);
+  Serial.println(" C");
+
 }
 
 void setPWMDuty(float duty){
-  //OCR1A = (word) (duty*TCNT1_TOP)/100;
-  //analogWrite(GTIOC7B_PIN, (int) (duty*TCNT1_TOP)/100);
   pwm.pulse_perc(duty);
 }
 
@@ -77,14 +89,14 @@ void fanControl() {
   //Above 40C, the fan runs at full speed
   //In between, the fan runs at a relative value between 20 and 100%
 
-  if (temp < Temp_Lower) {
+  if (avgTemp < TEMP_MIN) {
     PWMDuty = 20.0;
   } 
-  else if (temp > Temp_Upper) {     
+  else if (avgTemp > TEMP_MAX) {     
     PWMDuty = 100.0;
   }
   else {
-    PWMDuty = 20 + ((temp - Temp_Lower) / (Temp_Upper - Temp_Lower) * 80);
+    PWMDuty = 20 + ((avgTemp - TEMP_MIN) / (TEMP_MAX - TEMP_MIN) * 80);
   }
   PWMDuty_int = static_cast<int>(PWMDuty);
 
@@ -96,11 +108,11 @@ void fanControl() {
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
   temperature();
+  update_avg_temp();
   fanControl();
-  delay(500);
+  Serial.println("");
+  delay(1000);
 
 
 }
