1232cdd115d7c2f595f50e1df720d002acf3d439
Billy M <Billymortimer1965@gmail.com>
2025-02-10 19:11:45 +0000

    Added fan ramp limiting

--- Fan_Control/Fan_Control.ino
+++ Fan_Control/Fan_Control.ino
@@ -1,4 +1,14 @@
 #include <pwm.h>
+#include "Arduino_LED_Matrix.h"
+
+// Initialises the LED matrix used to display temperature and fan speed.
+ArduinoLEDMatrix matrix;
+
+unsigned long matrixFrame[] = { // 12 rows, 8 columns, 92 bits total. 3 nibbles per row
+  0x10101010
+  0x10101010
+  0x10101010
+};
 
 // Temperature control variables
 const int thermistorPin = 0; //Pin of Thermistor output
@@ -7,24 +17,32 @@ const float r1 = 10000; // Fixed resistor value, 10,000 Ohms
 float logR2, r2, temp, avgTemp;
 const float A = 1.009249522e-03, B = 2.378405444e-04, C = 2.019202697e-07; //Stein Heart Equation Values
 
-const int RT_Len = 10;
+const int RT_Len = 10; // Length of rolling temp list
+// higher RT_Lens mean transient temperature changes are more smoothed out. 
 float rollingTemp[RT_Len];
 int tempIndex = 0;
-//float rolling_temp[RT_Len] = {0.0, 0.0, 0.0, 0.0, 0.0};
-//std::fill(std::begin(rolling_temp), std::end(rolling_temp), 0);
 
 
 // Fan control variables
 const word PWM_FREQ_HZ = 25000;
 PwmOut pwm(D9);
 
-
-
-
 const int TEMP_MIN = 2;
 const int TEMP_MAX = 40;
 float PWMDuty;
+float Target_PWM;
 int PWMDuty_int;
+int Target_PWMDuty_int;
+
+unsigned long time; //millis()
+unsigned long prev_time;
+const unsigned long Fan_control = 1000;
+
+const float PWMDuty_Max_Change = 10 / (Fan_Control / 1000); //Maximum PWM_Duty change per second, in percent 
+
+//Add Ramp Rate
+//Time to ramp  - 8 seconds? and ramp increase per step? 10%
+//Requires recording previous PWMDuty and adding 5%  or last few %
 
 
 
@@ -38,10 +56,11 @@ void setup() {
   Serial.print("Program Start");
   Serial.println("");
 
-
-  //for (int i=0; i < RT_Len; i++) rolling_temp[i] = 0;
-
   std::fill(std::begin(rollingTemp), std::end(rollingTemp), 0);
+
+  prev_time = millis();
+
+  matrix.begin();
 
 }
 
@@ -85,34 +104,57 @@ void setPWMDuty(float duty){
 void fanControl() {
   //Change the PWM duty cycle to control fan speed
   //Changes speed based on temperature
-  //Below 20C, the fan runs at minimum speed
-  //Above 40C, the fan runs at full speed
+  //Below TEMP_MIN C, the fan runs at minimum speed
+  //Above TEMP_MAX C, the fan runs at full speed
   //In between, the fan runs at a relative value between 20 and 100%
 
   if (avgTemp < TEMP_MIN) {
-    PWMDuty = 20.0;
+    Target_PWMDuty = 20.0;
   } 
   else if (avgTemp > TEMP_MAX) {     
-    PWMDuty = 100.0;
+    Target_PWMDuty = 100.0;
   }
   else {
-    PWMDuty = 20 + ((avgTemp - TEMP_MIN) / (TEMP_MAX - TEMP_MIN) * 80);
+    Target_PWMDuty = 20 + ((avgTemp - TEMP_MIN) / (TEMP_MAX - TEMP_MIN) * 80);
   }
+
+  if (abs(Target_PWMDuty - PWMDuty) > PWMDuty_Max_Change) {
+
+    if (Target_PWMDuty > PWMDuty) {
+      PWM_Duty += PWMDuty_Max_Change;
+    } else {
+      PWM_Duty -= PWMDuty_Max_Change;
+    }
+
+  } else {
+    PWM_Duty = Target_PWMDuty;
+  }
+  setPWMDuty(PWMDuty);
+
   PWMDuty_int = static_cast<int>(PWMDuty);
+  Target_PWMDuty_int = static_cast<int>(Target_PWMDuty);
 
   setPWMDuty(PWMDuty);
   Serial.print("PWM Duty: ");
   Serial.println(PWMDuty_int);
+  Serial.print("Target PWM Duty: ");
+  Serial.println(Target_PWMDuty_int);
 }
 
 
 
 void loop() {
-  temperature();
-  update_avg_temp();
-  fanControl();
-  Serial.println("");
+
+  time = millis();
+  if (prev_time - time > Fan_Interval) {
+    prev_time = time;
+    temperature();
+    update_avg_temp();
+    fanControl();
+  }
+
   delay(1000);
+  Serial.println("");
 
 
 }
