414d3795ca9bfd2819cf50a1364ec221f342a6f9
Billy M <Billymortimer1965@gmail.com>
2025-02-11 16:29:05 +0000

    Added reading of fan RPM from fan tachometer

--- Fan_Control/Fan_Control.ino
+++ Fan_Control/Fan_Control.ino
@@ -6,9 +6,9 @@ ArduinoLEDMatrix matrix;
 
 
 unsigned long matrixFrame[] = { // 12 rows, 8 columns, 92 bits total. 3 nibbles per row
-  0x00000000,
-  0x00000000,
-  0x00000000
+  0x00000000, //Row 1, 2, 3
+  0x00000000, //Row 3, 4, 5, 6
+  0x00000000 //Row 6, 7, 8
 };
 
 unsigned short PWM_Target_LED = 0x0FFF, PWM_LED = 0x0FFF, Temp_LED = 0x0FFF;
@@ -38,15 +38,19 @@ float Target_PWMDuty;
 int PWMDuty_int;
 int Target_PWMDuty_int;
 
-unsigned long curTime; //millis()
+unsigned long curTime; 
 unsigned long prevTime;
 const unsigned long Fan_Interval = 1000; // Time between loop iterations in milliseconds
 
 const float PWMDuty_Max_Change = 10 / (Fan_Interval / 1000); //Maximum PWM_Duty change per second, in percent 
 
-//Add Ramp Rate
-//Time to ramp  - 8 seconds? and ramp increase per step? 10%
-//Requires recording previous PWMDuty and adding 5%  or last few %
+
+// Reading fan speed from PWM fan:
+const int TACHPIN = 2;
+const int DEBOUNCE = 0;
+const int FANSTUCK_THRESHOLD = 500000;
+unsigned long volatile ts1=0, ts2=0; //Time stamps for interrupts
+unsigned long fanRPM = 0;
 
 
 
@@ -56,22 +60,24 @@ void setup() {
   pwm.begin(PWM_FREQ_HZ, 20.0f);
   PWMDuty = 20.0;
 
-  std::fill(std::begin(rollingTemp), std::end(rollingTemp), 0);
+  std::fill(std::begin(rollingTemp), std::end(rollingTemp), 0); // Initialises rolling temp array
 
   prevTime = millis();
   
+
+  // Initialises the LED Matrix
+
   matrix.begin();
-
-  //matrixFrame[0] = 0x0 | PWM_LED << 4 | PWM_LED << 16;
-  //matrixFrame[1] = PWM_Target_LED | PWM_Target_LED << 12 | 0x00;
-  //matrixFrame[2] = Temp_LED >> 4| Temp_LED << 8 | 0x00 << 24;
-
   matrixFrame[0] = PWM_LED << 8 | PWM_LED << 20;
   matrixFrame[1] = PWM_Target_LED << 4| PWM_Target_LED << 16;
   matrixFrame[2] = Temp_LED | Temp_LED << 12;
-
   matrix.loadFrame(matrixFrame);
 
+  //Fan RPM Reading setup
+  pinMode(TACHPIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(TACHPIN), tachISR, FALLING);
+
+  // Delay before program starts
   delay(3000);
 
   Serial.print("Program Start");
@@ -80,8 +86,42 @@ void setup() {
 
 }
 
+void tachISR(){ 
+  //Interrupt Handler, stores the time stamp of previous 2 interrupts and handles debouncing
+  unsigned long m=micros();
+  if((m-ts2) > DEBOUNCE){
+    ts1=ts2;
+    ts2=m;
+  }
+}
+
+void calcRPM(){
+  //Known bug, fan RPM sometimes reads as double of true value. Forum posts discuss similar issues with other implemenations. 
+  //Calculates the fan RPM from interrupts on pin 2
+  unsigned long ts1_copy, ts2_copy;
+
+  //Temporarily disable interrupts and copy ts1 and ts2 to avoid values being overwritten while in use
+  noInterrupts();
+  ts1_copy = ts1;
+  ts2_copy = ts2;
+  interrupts();
+
+  if(micros()-ts2_copy < FANSTUCK_THRESHOLD && ts2_copy != 0){
+    fanRPM = (60000000 / (ts2_copy-ts1_copy)/2);
+  } else fanRPM = 0;
+
+  Serial.print("Fan RPM: ");
+  Serial.println(fanRPM);
+}
+
+
+
+
+
+
 
 unsigned short percentToMatrix(int percent){
+  //Converts a percantage to a value for lighting the correct number of LEDS in the matrix. 
   int scale = (int)(percent * 1.2 / 10);
   unsigned short output = 0;
   for (int i=0; i < scale; i++) output += pow(2, i);
@@ -195,6 +235,7 @@ void loop() {
     update_avg_temp();
     fanControl();
     matrixControl();
+    calcRPM();
   }
 
   delay(1000);
